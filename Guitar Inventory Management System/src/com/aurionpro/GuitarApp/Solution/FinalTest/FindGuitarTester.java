6423cbc51d602d92ecb9c00acdec92fbe0c1ac95
Anshika Dubey <150266045+DevStudyBug@users.noreply.github.com>
2025-07-24 10:38:18 +0530

    Delete: old solution

--- Guitar Inventory Management System/src/com/aurionpro/GuitarApp/Solution/FinalTest/FindGuitarTester.java
+++
@@ -1,8 +0,0 @@
-package com.aurionpro.GuitarApp.Solution.FinalTest;
-
-public class FindGuitarTester {
-    public static void main(String[] args) {
-        GuitarUIDesign ui = new GuitarUIDesign();
-        ui.start();
-    }
-}
